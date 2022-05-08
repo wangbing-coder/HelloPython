55b97b11a5e35073758542ab2186cc90c6a59ef0
Bing Wang <bing.wang@derbysoft.net>
2022-05-08 15:50:55 +0800

    t

--- pp.py
+++ pp.py
@@ -4,6 +4,7 @@ from pandas_profiling import ProfileReport
 
 df = pd.DataFrame(np.random.rand(100, 5), columns=["a", "b", "c", "d", "e"])
 
+
 print(df)
 #profile = ProfileReport(df, title="Pandas Profiling Report")
 #profile.to_file("my_report.html")
